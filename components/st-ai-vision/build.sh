ef74522c9cea7e9894886bc77d81a0fea47b290e
nik-markovic <nikola.m@gmail.com>
2025-06-04 16:46:11 -0500

    Working Component

--- components/st-ai-vision/build.sh
+++ components/st-ai-vision/build.sh
@@ -4,15 +4,8 @@ set -e
 
 cd "$(dirname "$0")"
 
-if which gdk > /dev/null; then
+if ! which gdk > /dev/null; then
   python3 -m pip install -U git+https://github.com/aws-greengrass/aws-greengrass-gdk-cli.git@v1.6.2
-fi
-
-if [[ -n "$PACKAGE_LOCAL_SDK" ]]; then
-  # Optional: Set this value to anything to build and package local SDK source as well.
-  ../../scripts/package.sh
-  mkdir -p local-packages/
-  cp -f ../../dist/*.whl local-packages/
 fi
 
 gdk component build
--- components/st-ai-vision/wrapper/system-setup.sh
+++ components/st-ai-vision/wrapper/system-setup.sh
@@ -1,4 +1,11 @@
-apt-get update
-apt-get install x-linux-ai-tool -y
+#!/bin/bash
+
+set -e
+
+# For "Inappropriate ioctl for device":
+export DEBIAN_FRONTEND=noninteractive
+
+apt update -y
+apt install -y x-linux-ai-tool
 x-linux-ai -i packagegroup-x-linux-ai-demo-cpu
 systemctl restart weston-graphical-session.service
