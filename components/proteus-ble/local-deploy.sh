cdeb879bcdc4a4a3b3471605d91e34f2ff1888ad
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-08-07 13:12:30 -0600

    Removed CPID/ENV config sections of local-deploy.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- components/proteus-ble/local-deploy.sh
+++ components/proteus-ble/local-deploy.sh
@@ -20,27 +20,3 @@ sudo -E /greengrass/v2/bin/greengrass-cli \
   --merge "io.iotconnect.example.IotConnectSdkProteusBleDemo=1.0.0"
 
 exit 0
-
-# DEVELOPMENT NOTE:
-# This pattern can be used instead of the line above to inject runtime component configuration:
-
-# shellcheck disable=SC2317
-IOTC_CONFIG=$(cat <<EOF
-{
-  "io.iotconnect.example.IotConnectSdkProteusBleDemo": {
-     "MERGE": {
-      "IOTC_CPID": "${IOTC_CPID}",
-      "IOTC_ENV": "${IOTC_ENV}"
-    }
-  }
-}
-EOF
-)
-
-# shellcheck disable=SC2317
-sudo -E /greengrass/v2/bin/greengrass-cli \
-deployment create \
---recipeDir ${PWD}/greengrass-build/recipes \
---artifactDir ${PWD}/greengrass-build/artifacts \
---merge "io.iotconnect.example.IotConnectSdkProteusBleDemo=1.0.0" \
---update-config "$IOTC_CONFIG"
