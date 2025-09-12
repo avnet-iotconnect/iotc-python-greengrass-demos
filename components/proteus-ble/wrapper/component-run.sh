6cb8f98f9ee578450644312b17e2396b5a0f0831
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-09-12 11:47:45 -0600

    Delete components directory
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- components/proteus-ble/wrapper/component-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "$(dirname "$0")"/..
-
-source ~/.venv-proteus-ble-demo/bin/activate
-python3 -u proteus-ble.py "$@"
--- components/sensortileboxpro-ble/wrapper/component-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "$(dirname "$0")"/..
-
-source ~/.venv-sensortileboxpro-ble-demo/bin/activate
-python3 -u sensortileboxpro-ble.py "$@"
